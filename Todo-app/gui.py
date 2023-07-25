78d3ffb09e30a7ba9003d46b2f0f5ea9f6c6cbe8
rafay <rafitahmix@gmail.com>
2023-07-25 18:08:37 -0400

    Implement the Add button

--- Todo-app/gui.py
+++ Todo-app/gui.py
@@ -5,7 +5,7 @@ label = sg.Text("Type in a todo: ")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True,   size=[45, 10])
+                      enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 
 
