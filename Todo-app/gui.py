9d9313143fcbe40f32e37c2fad8315c81822095e
rafay <rafitahmix@gmail.com>
2023-07-27 13:54:32 -0400

    Implement the Add button

--- Todo-app/gui.py
+++ Todo-app/gui.py
@@ -8,10 +8,14 @@ list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                    layout=[[label], [input_box,add_button],[list_box, edit_button]],
+                    layout=[[label],
+                    [input_box,add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=('Helvetica',15))
 while True:
     event,values = window.read()
@@ -36,5 +40,14 @@ while True:
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(values='')
+        case "Exit":
+            break
 
 window.close()
