27c4e083e78f949230a9bb5a2ecc06624b915bcd
rafay <rafitahmix@gmail.com>
2023-07-29 14:50:49 -0400

    Create a web app

---
+++ web.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import functions
+todos = functions.get_todos()
+st.title("My Todo app")
+st.subheader("This is my todo app")
+st.write("This app is to improve your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Add new todo")
+
